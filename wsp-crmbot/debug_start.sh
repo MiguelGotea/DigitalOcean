ccec56b4475e28b4652de2796e23c513e4dbb6b4
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-23 08:08:25 -0600

    Fixed

--- wsp-crmbot/debug_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DEBUG='whatsapp-web.js*'
-export PORT=3001
-cd /var/www/wsp-crmbot
-node src/app.js > debug_protocol.log 2>&1 &
-echo $! > debug.pid
-echo "Debug process started with PID $(cat debug.pid)"
--- wsp-crmbot/run_test_wpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /var/www/wsp-crmbot
-export DEBUG='whatsapp-web.js*'
-node test_wpp.js > test_output.log 2>&1
