8a8a38d58aaa5bc719a656821dfb72ffa09d838c
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-23 08:00:21 -0600

    Add

--- wsp-crmbot/test_wpp.js
+++ wsp-crmbot/test_wpp.js
@@ -4,11 +4,24 @@ const fs = require('fs');
 console.log('🚀 Starting minimal WPP test...');
 
 const client = new Client({
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1018911162-alpha.html'
+    },
     puppeteer: {
         headless: 'new',
         executablePath: '/usr/bin/google-chrome-stable',
         dumpio: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-extensions'
+        ]
     }
 });
 
